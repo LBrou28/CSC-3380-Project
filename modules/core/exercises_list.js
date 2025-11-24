36e80d86f70520d73aaed96616c8247757ceb725
Luke Broussard <lukemb28@gmail.com>
2025-11-24 14:25:54 -0600

    Add new exercises to exercises_list.js

--- modules/core/exercises_list.js
+++ modules/core/exercises_list.js
@@ -4,25 +4,48 @@
 export const EXERCISES = [
     // Chest
     { name: "Push-Ups", group: "Chest" },
+    { name: "Bench Press", group: "Chest" },
+    { name: "Incline Chest Press", group: "Chest" },
+    { name: "DB Bench Press", group: "Chest" },
 
     // Back
     { name: "Pull-Ups", group: "Back" },
+    { name: "Barbell Rows", group: "Back" },
+    { name: "Cable Rows", group: "Back" },
+    { name: "Pull-Ups", group: "Back" },
+    { name: "Lat PullDowns", group: "Back" },
+    
 
     // Legs
     { name: "Bodyweight Squat", group: "Legs" },
     { name: "Goblet Squat", group: "Legs" },
+    { name: "Quad Extensions", group: "Legs" },
+    { name: "Hamstring Curls", group: "Legs" },
+    { name: "Calf Raises", group: "Legs" },
+    { name: "RDLS", group: "Legs" },
+    
 
     // Shoulders
     { name: "Overhead Press", group: "Shoulders" },
     { name: "Lateral Raise", group: "Shoulders" },
+    { name: "DB Shoulder Press", group: "Shoulders" },
+    { name: "Rear Delt Flys", group: "Shoulders" },
+    
 
     // Arms
     { name: "Bicep Curls", group: "Arms" },
     { name: "Triceps Dips", group: "Arms" },
+    { name: "Triceps Extensions", group: "Arms" },
+    { name: "Over-Head Tricep Extensions", group: "Arms" },
+    { name: "EZ Bar Bicep Curls", group: "Arms" },
+    { name: "Incline Bicep Curls", group: "Arms" },
+    { name: "Spider Curls", group: "Arms" },
 
     // Core
     { name: "Plank", group: "Core" },
     { name: "Russian Twist", group: "Core" },
+    { name: "Sit-ups", group: "Core" },
+    { name: "Leg Raises", group: "Core" },
 
     // Full Body
     { name: "Burpees", group: "Full Body" },
