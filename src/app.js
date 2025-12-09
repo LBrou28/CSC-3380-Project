52f798b1aace987153d22a56b56ad916205a0ad4
phong54321 <phonghuong2033@gmail.com>
2025-12-09 10:36:10 -0600

    move app.js again

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 /*
 	The app, currently only generates workouts AND More
 */
-import * as workout_generator from "/src/features/workout_generator.js"
-import * as stopwatch from "/src/features/stopwatch.js"
-import * as saved_workouts from "/src/features/saved_workouts.js"
-import * as notes from "/src/features/notes.js"
+import * as workout_generator from "/modules/features/workout_generator.js"
+import * as stopwatch from "/modules/features/stopwatch.js"
+import * as saved_workouts from "/modules/features/saved_workouts.js"
+import * as notes from "/modules/features/notes.js"
+import * as weekly_planner from "/modules/features/weekly_planner.js"
 
 workout_generator.init()
 stopwatch.init()
 saved_workouts.init()
 notes.init()
+weekly_planner.init()
